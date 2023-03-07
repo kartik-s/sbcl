cf6cb6d0b64ac122fa71dec0a9b338a91330490c
Kartik Singh <kartik@freelygenerated.com>
2023-03-06 18:57:22 -0800

    bro

--- src/runtime/main.c
+++ src/runtime/main.c
@@ -4,8 +4,8 @@
 
 struct fiber_data {
     int argc;
-    char *argv[];
-    char *envp;
+    char **argv;
+    char **envp;
     void *alien_fiber;
 };
 
@@ -13,7 +13,7 @@ void init(void *p)
 {
     struct fiber_data *data = p;
     extern int initialize_lisp(int argc, char *argv[], char *envp[]);
-    initialize_lisp(argc, argv, envp);
+    initialize_lisp(data->argc, data->argv, data->envp);
     lose("unexpected return from initial thread in main()");
 }
 
@@ -24,7 +24,7 @@ int main(int argc, char *argv[], char *envp[])
     data.argc = argc;
     data.argv = argv;
     data.envp = envp;
-    data.alien_fiber = ConvertThreadToFiber();
+    data.alien_fiber = ConvertThreadToFiber(NULL);
 
     void *lisp_fiber = CreateFiber(0, init, &data);
     SwitchToFiber(lisp_fiber);
