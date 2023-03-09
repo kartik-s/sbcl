8c0395d1721f85ca4b6ee8d2ffa653713a109e5b
Kartik Singh <kssingh@hrl.com>
2023-03-08 19:03:09 -0800

    dataref it

--- src/compiler/x86-64/c-call.lisp
+++ src/compiler/x86-64/c-call.lisp
@@ -555,8 +555,7 @@
 
         #+sb-thread
         (progn
-          (inst mov rcx (ea (foreign-symbol-address "sbcl_thread_tls_index")))
-          (inst mov rcx (ea rcx))
+          (inst mov rcx (ea (foreign-symbol-address "sbcl_thread_tls_index" t)))
           (inst call (ea (foreign-symbol-address "TlsGetValue")))
           (inst mov thread-tn rax)
           ;; arg0 to ENTER-ALIEN-CALLBACK (trampoline index)
