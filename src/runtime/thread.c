8833495f71cbc50e18ed5f6fe0b15d2c148c7037
Kartik Singh <kartik@freelygenerated.com>
2023-09-06 12:21:30 -0400

    oops

--- src/runtime/thread.c
+++ src/runtime/thread.c
@@ -828,6 +828,7 @@ callback_wrapper_trampoline(
     if (!th) {                  /* callback invoked in non-lisp thread */
         init_thread_data scribble;
         attach_os_thread(&scribble);
+        th = get_sb_vm_thread();
         pthread_setspecific(foreign_thread_ever_lispified, (void *) 1);
 
         WITH_GC_AT_SAFEPOINTS_ONLY()
