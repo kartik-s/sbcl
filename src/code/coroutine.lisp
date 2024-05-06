9df3ab51eef67569fbd3c79cedbff7d72792877d
Kartik Singh <kartik@freelygenerated.com>
2024-05-06 19:59:37 +0200

    WIP stack fork VOP

--- src/code/coroutine.lisp
+++ src/code/coroutine.lisp
@@ -1,14 +1,100 @@
+(in-package "SB-VM")
+
+(export 'control-stack-fork)
+(defknown control-stack-fork ((simple-array (unsigned-byte 64) (*)) )
+  (member t nil))
+
+(define-vop (control-stack-fork)
+  (:policy :fast-safe)
+  (:translate control-stack-fork)
+  (:args (save-stack :scs (descriptor-reg) :to :result))
+  (:arg-types simple-array-unsigned-byte-64)
+  (:results (child :scs (descriptor-reg)))
+  (:result-types t)
+  (:temporary (:sc unsigned-reg :from (:eval 0) :to (:eval 1)) index)
+  (:temporary (:sc unsigned-reg :from (:eval 0) :to (:eval 1)) stack)
+  (:temporary (:sc unsigned-reg :from (:eval 0) :to (:eval 1)) temp)
+  (:save-p t)
+  (:generator 25
+    ;; Setup the return context.             
+    (inst adr temp return)
+    (inst str temp (@ csp-tn))
+    (inst add csp-tn csp-tn sb-vm:n-word-bytes)
+    (load-foreign-symbol stack "control_stack_end" :dataref t)
+    (inst ldr stack (@ stack))
+    ;; New FP is the Top of the stack.
+    (inst str stack (@ csp-tn))
+    (inst add csp-tn csp-tn sb-vm:n-word-bytes)
+    ;; Save the stack.
+    (inst xor index index)
+    ;; First save the adjusted stack-pointer.
+    (inst add stack cfp-tn)
+    (inst sub stack csp-tn)
+    (inst add temp save-stack (lsl index word-shift))
+    (storew stack temp sb-vm:vector-data-offset sb-vm:other-pointer-lowtag)
+    ;; Save the current frame, replacing the OCFP and RA by 0.
+    (storew 0 temp (+ 1 sb-vm:vector-data-offset) sb-vm:other-pointer-lowtag)
+    ;; Save 0 for the OCFP.
+    (storew 0 temp (+ 2 sb-vm:vector-data-offset) sb-vm:other-pointer-lowtag)
+    (inst add index index 3)
+    ;; Copy the remainder of the frame, skiping the OCFP and RA which
+    ;; are saved above.
+    (inst ldr stack (@ cfp-tn (* -2 sb-vm:n-word-bytes)))
+
+    LOOP
+    (inst cmp stack esp-tn)
+    (inst jmp :le stack-save-done)
+    (inst sub stack 4)
+    (inst mov temp (make-ea :dword :base stack))
+    (inst mov (make-ea :dword :base save-stack :index index :scale 4
+		       :disp (- (* vm:vector-data-offset vm:word-bytes)
+				vm:other-pointer-type))
+	  temp)
+    (inst inc index)
+    (inst jmp-short LOOP)
+    
+    RETURN
+    ;; Stack already clean if it reaches here. Parent returns NIL.
+    (inst mov child nil-value)
+    (inst jmp-short DONE)
+    
+    STACK-SAVE-DONE
+    ;; Cleanup the stack
+    (inst add esp-tn 8)
+    ;; Child returns T.
+    (load-symbol child t)
+    DONE))
+
+(defknown control-stack-swap ((simple-array (unsigned-byte 64) (*))
+                              (simple-array (unsigned-byte 64) (*)))
+    (values))
+
+(define-vop (control-stack-swap)
+  (:policy :fast-safe)
+  (:translate control-stack-swap)
+  (:args (resumer-stack :scs (descriptor-reg) :to :result)
+         (resumee-stack :scs (descriptor-reg) :to :result))
+  (:temporary (:sc unsigned-reg) jump-addr)
+  (:arg-types simple-array-unsigned-byte-64 simple-array-unsigned-byte-64)
+  (:save-p t)
+  (:generator 25
+              (inst str csp-tn (@ resumer-stack (* 0 sb-vm:n-word-bytes)))
+              (inst ldr csp-tn (@ resumee-stack (* 0 sb-vm:n-word-bytes)))
+              (inst ldr jump-addr (@ csp-tn (* -1 sb-vm:n-word-bytes)))
+              (inst ret jump-addr)))
+
+
 (defpackage "SB-COROUTINE"
   (:documentation "Coroutines!!!!!!")
   (:use "CL"))
 
 (in-package "SB-COROUTINE")
 
-(declaim (type (simple-array (or null (simple-array (unsigned-byte 32) (*)))
+(declaim (type (simple-array (or null (simple-array (unsigned-byte 64) (*)))
                              (*)) *control-stacks*))
 (defvar *control-stacks*
-  (make-array 0 :element-type '(or null (unsigned-byte 32))
-                :initial-element nil))
+  (make-array 10 :element-type '(or null (unsigned-byte 64))
+                 :initial-element nil))
 
 (defstruct (coroutine
             (:constructor %make-coroutine)
@@ -21,11 +107,12 @@
                  (format stream "Coroutine ~a, ~a"
                          (sb-kernel:get-lisp-obj-address coroutine)
                          (coroutine-state coroutine))))))
+  trampoline
   (state :inactive :type (member :active :inactive))
   (control-stack-id nil :type (or sb-kernel::index null))
   (current-catch-block 0 :type fixnum)
   (current-unwind-protect-block 0 :type fixnum)
-  (alien-stack nil :type (or (simple-array (unsigned-byte 32) (*)) null))
+  (alien-stack nil :type (or (simple-array (unsigned-byte 64) (*)) null))
   (alien-stack-pointer 0 :type (unsigned-byte 32))
   (resumer nil :type (or coroutine null)))
 
@@ -56,7 +143,7 @@
                   ;; push onto the bottom of the stack.
                   (control-stack
                     (make-array (+ (ceiling control-stack-size 4) 3)
-                                :element-type '(unsigned-byte 32)
+                                :element-type '(unsigned-byte 64)
                                 :initial-element 0)))
              (declare (type (unsigned-byte 29) control-stack-size))
              (unless control-stack-id
@@ -65,7 +152,7 @@
                (setf *control-stacks*
                      (adjust-array *control-stacks*
                                    (* 2 (length *control-stacks*))
-                                   :element-type '(or null (unsigned-byte 32))
+                                   :element-type '(or null (unsigned-byte 64))
                                    :initial-element nil)))
              (setf (aref *control-stacks* control-stack-id) control-stack)
              (values control-stack control-stack-id)))
@@ -77,8 +164,8 @@
             :control-stack-id control-stack-id
             :current-catch-block 0
             :current-unwind-protect-block 0
-            :alien-stack (make-array 0 :element-type '(unsigned-byte 32))
-            :alien-stack-pointer *alien-stack-top*
+            :alien-stack (make-array 0 :element-type '(unsigned-byte 64))
+            :alien-stack-pointer 0
             :resumer nil)))
     (let ((child-coroutine nil))
       (sb-sys:without-interrupts
@@ -91,8 +178,13 @@
                       (unwind-protect
                            (funcall initial-function)
                         (format t "uh oh the stack unwound!!!!! uh oh")))))
+              (format t "~a~%" (sb-vm:simple-fun-entry-sap trampoline))
+              (disassemble trampoline)
+              (setf (aref control-stack 0)
+                    (+ (sb-kernel:get-lisp-obj-address control-stack)
+                       (* 2 sb-vm:n-word-bytes)))
               (sb-sys:with-pinned-objects (trampoline)
-                (setf (aref control-stack 1)
+                (setf (aref contronl-stack 1)
                       (sb-sys:sap-int (sb-vm:simple-fun-entry-sap trampoline))))))))
       child-coroutine)))
 
@@ -101,108 +193,37 @@
            (optimize (speed 3)))
   (assert (and (eq (coroutine-state resumee) :active)
                (not (eq resumee *current-coroutine*))))
-  (sb-sys:without-interrupts
-    (sb-sys:without-gcing
-      (let* (;; Save the current coroutine on its stack.
-             (resumer *current-coroutine*)
-             ;; The save-stack vector.
-             (resumer-control-stack (aref *control-stacks*
-                                          (coroutine-control-stack-id resumer))))
-        (declare (type (simple-array (unsigned-byte 32) (*)) resumer-control-stack))
-        ;; Misc stacks.
-        (setf (coroutine-current-catch-block resumer)
-              sb-vm::*current-catch-block*)
-        (setf (coroutine-current-unwind-protect-block resumer)
-              sb-vm::*current-unwind-protect-block*)
-        (setf (coroutine-alien-stack-pointer resumer)
-              sb-vm::*current-alien-stack-pointer*)
-        (setf sb-vm::*current-catch-block*
-              (coroutine-current-catch-block resumee))
-        (setf sb-vm::*current-unwind-protect-block*
-              (coroutine-current-unwind-protect-block resumee))
-        (setf sb-vm::*alien-stack-pointer*
-              (coroutine-alien-stack-pointer resumee))
+  (sb-sys:without-gcing
+    (let* (;; Save the current coroutine on its stack.
+           (resumer *current-coroutine*)
+           ;; The save-stack vector.
+           (resumer-control-stack
+             (let ((stack-id (coroutine-control-stack-id resumer)))
+               (if (null stack-id)
+                   (make-array 1024 :element-type '(unsigned-byte 64))
+                   (aref *control-stacks* stack-id)))))
+      (declare (type (simple-array (unsigned-byte 64) (*)) resumer-control-stack))
+      ;; Misc stacks.
+      (setf (coroutine-current-catch-block resumer)
+            sb-vm::*current-catch-block*)
+      (setf (coroutine-current-unwind-protect-block resumer)
+            sb-vm::*current-unwind-protect-block*)
+      (setf (coroutine-alien-stack-pointer resumer)
+            sb-vm::*alien-stack-pointer*)
+      (setf sb-vm::*current-catch-block*
+            (coroutine-current-catch-block resumee))
+      (setf sb-vm::*current-unwind-protect-block*
+            (coroutine-current-unwind-protect-block resumee))
+      (setf sb-vm::*alien-stack-pointer*
+            (coroutine-alien-stack-pointer resumee))
 
-        (let ((resumee-control-stack
-                (aref *control-stacks*
-                      (coroutine-control-stack-id resumee))))
-          (declare (type (simple-array (unsigned-byte 32) (*))
-                         resumee-control-stack))
-          (control-stack-swap resumer-control-stack resumee-control-stack))
-        ;; Thread returns.
-        (setq *current-coroutine* resumer))))
+      (let ((resumee-control-stack
+              (aref *control-stacks*
+                    (coroutine-control-stack-id resumee))))
+        (declare (type (simple-array (unsigned-byte 64) (*))
+                       resumee-control-stack))
+        (format t "about to swap control stacks~%")
+        (sb-vm::control-stack-swap resumer-control-stack resumee-control-stack))
+      ;; Thread returns.
+      (setq *current-coroutine* resumer)))
   (values))
-
-;;;; VOPs
-
-(sb-c:defknown control-stack-swap ((simple-array (unsigned-byte 32) (*))
-                                   (simple-array (unsigned-byte 32) (*)))
-    (values))
-
-(define-vop (control-stack-swap)
-  (:policy :fast-safe)
-  (:translate control-stack-swap)
-  (:args (resumer-stack :scs (descriptor-reg) :to :result)
-         (resumee-stack :scs (descriptor-reg) :to :result))
-  (:arg-types simple-array-unsigned-byte-32 simple-array-unsigned-byte-32)
-  (:temporary (:sc unsigned-reg :from (:eval 0) :to (:eval 1)) index)
-  (:temporary (:sc unsigned-reg :from (:eval 0) :to (:eval 1)) stack)
-  (:temporary (:sc unsigned-reg :from (:eval 0) :to (:eval 1)) temp)
-  (:save-p t)
-  (:generator 25
-              ;; Setup the return context.
-              (inst push (make-fixup nil :code-object RETURN))
-              (inst push ebp-tn)
-              ;; Save the stack.
-              (inst xor index index)
-              ;; First the stack-pointer.
-              (inst mov (make-ea :dword :base save-stack :index index :scale 4
-                                        :disp (- (* vm:vector-data-offset vm:word-bytes)
-                                                 vm:other-pointer-type))
-                    esp-tn)
-              (inst inc index)
-              (load-foreign-data-symbol stack "control_stack_end")
-              (inst mov stack (make-ea :dword :base stack))
-              LOOP
-              (inst cmp stack esp-tn)
-              (inst jmp :le STACK-SAVE-DONE)
-              (inst sub stack 4)
-              (inst mov temp (make-ea :dword :base stack))
-              (inst mov (make-ea :dword :base save-stack :index index :scale 4
-                                        :disp (- (* vm:vector-data-offset vm:word-bytes)
-                                                 vm:other-pointer-type))
-                    temp)
-              (inst inc index)
-              (inst jmp-short LOOP)
-
-              STACK-SAVE-DONE
-              ;; Cleanup the stack
-              (inst add esp-tn 8)
-
-              ;; Restore the new-stack.
-              (inst xor index index)
-              ;; First the stack-pointer.
-              (inst mov esp-tn
-                    (make-ea :dword :base new-stack :index index :scale 4
-                                    :disp (- (* vm:vector-data-offset vm:word-bytes)
-                                             vm:other-pointer-type)))
-              (inst inc index)
-              (load-foreign-data-symbol stack "control_stack_end")
-              (inst mov stack (make-ea :dword :base stack))
-              LOOP2
-              (inst cmp stack esp-tn)
-              (inst jmp :le STACK-RESTORE-DONE)
-              (inst sub stack 4)
-              (inst mov temp (make-ea :dword :base new-stack :index index :scale 4
-                                             :disp (- (* vm:vector-data-offset vm:word-bytes)
-                                                      vm:other-pointer-type)))
-              (inst mov (make-ea :dword :base stack) temp)
-              (inst inc index)
-              (inst jmp-short LOOP2)
-              STACK-RESTORE-DONE
-              ;; Pop the frame pointer, and resume at the return address.
-              (inst pop ebp-tn)
-              (inst ret)
-
-              ;; Original thread resumes, stack has been cleaned up.
-              RETURN))
